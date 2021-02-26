5e1ef9990b088d5ac336557d1f5d5e84b0a233f5
Leopold Hock <kontakt@leopoldhock.de>
2021-02-26 16:12:30 +0100

    cors fix

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,9 @@ use App\Models\User;
 |
 */
 
-Route::group(["middleware" => "api-private"], function ($router) {
+Route::group([
+    "middleware" => "api"
+], function ($router) {
     JsonApi::register('default')->withNamespace('App\Http\Controllers\Api')->singularControllers()->routes(function ($api) {
         $api->resource('bug-reports');
         $api->resource("users")->relationships(function ($relations) {
@@ -28,7 +30,7 @@ Route::group(["middleware" => "api-private"], function ($router) {
 });
 
 Route::group([
-    'middleware' => 'api-private',
+    'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', 'App\Http\Controllers\AuthController@login');
@@ -36,33 +38,6 @@ Route::group([
     Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
     Route::post('me', 'App\Http\Controllers\AuthController@me');
 });
-
-// Route::post('auth/login', 'App\Http\Controllers\AuthController@login');
-// Route::post('auth/logout', 'App\Http\Controllers\AuthController@logout');
-// Route::post('auth/refresh', 'App\Http\Controllers\AuthController@refresh');
-// Route::post('auth/me', 'App\Http\Controllers\AuthController@me');
-
-
-// // get csrf token
-// Route::get('/csrf-cookie', function (Request $request) {
-//     $token = $request->session()->token();
-//     $token = csrf_token();
-// });
-
-// // route for authorization using sanctum
-// Route::post('sanctum/auth', function (Request $request) {
-//     $request->validate([
-//         'email' => 'required|email',
-//         'password' => 'required'
-//     ]);
-//     $user = User::where('email', $request->email)->first();
-//     if (!$user || !Hash::check($request->password, $user->password)) {
-//         throw ValidationException::withMessages([
-//             "error" =>  ["invalid_grant"],
-//         ]);
-//     }
-//     return $user->createToken($request->email)->plainTextToken;
-// });
 
 Route::fallback(function () {
     return response()->json(["message" => "Route not found."], 404);
